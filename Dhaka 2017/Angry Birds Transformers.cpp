af30c40453ae6943e9c9ed3bfa351eaec55fc3d3
Ishraq_Nibir <ishraq.nibir@gmail.com>
2019-02-02 20:41:00 +0600

    Add files via upload

---
+++ Dhaka 2017/Angry Birds Transformers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define BASE 510
+#define MAXX (10000+500+BASE)
+int main() {
+    int n;
+    static int IN[MAXX], OUT[MAXX];
+    while (scanf("%d", &n) == 1 && n) {
+        memset(IN, 0, sizeof(IN));
+        memset(OUT, 0, sizeof(OUT));
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            OUT[x+y+BASE]++;
+            IN[x-y+BASE]++;
+        }
+
+        int ret = 0;
+        int sum = 0;
+        for (int i = 0; i < MAXX; i++) {
+            sum += IN[i];
+            ret = max(ret, sum);
+            sum -= OUT[i];
+        }
+        printf("%d\n", ret);
+    }
+    return 0;
+}
